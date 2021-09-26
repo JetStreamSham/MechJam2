e0c64e3412cd8b8e67046b11ac26adba0ccbda5c
JetStreamSham <76616771+JetStreamSham@users.noreply.github.com>
2021-09-26 14:02:21 -0500

    Update AudioEvent
    AudioEvent now supports multiple concurrent sounds

--- Assets/Scripts/AudioEvent.cs
+++ Assets/Scripts/AudioEvent.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class AudioEvent : MonoBehaviour
 {
 
-    public AudioSource source;
+    public int concurrentSounds = 4;
+    public AudioSource[] source;
     public AudioClip[] audioClips;
 
+
+    private void Start()
+    {
+        source = new AudioSource[concurrentSounds];
+        for (int i = 0; i < concurrentSounds; i++)
+        {
+            GameObject childObject = new GameObject($"{gameObject.name} Audio Source {i}");
+            childObject.transform.parent = transform;
+            childObject.transform.position = Vector3.zero;
+            childObject.transform.rotation = transform.rotation;
+            source[i] = childObject.AddComponent<AudioSource>();
+        }
+
+    }
     public void PlayClip(int clipID)
     {
-        source.clip = audioClips[clipID];
-        source.Play();
+        for(int i = 0; i < concurrentSounds; i++)
+        {
+            if (!source[i].isPlaying)
+            {
+                source[i].clip = audioClips[clipID];
+                source[i].Play();
+                break;
+            }
+        }
     }
 }
