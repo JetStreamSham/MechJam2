3ef048996f81a99448f5ae7d6f7b8b07171ed3d5
Civans <12769433+Civans@users.noreply.github.com>
2021-09-28 13:45:31 -0500

    Update AudioEvent.cs
    AudioSources in child objects will be used for animations events if found.

--- Assets/Scripts/AudioEvent.cs
+++ Assets/Scripts/AudioEvent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioEvent : MonoBehaviour
 {
@@ -9,23 +10,36 @@ public class AudioEvent : MonoBehaviour
     public AudioSource[] source;
     public AudioClip[] audioClips;
 
+    public AudioMixerGroup mixer;
 
     private void Start()
     {
-        source = new AudioSource[concurrentSounds];
-        for (int i = 0; i < concurrentSounds; i++)
+
+        source = GetComponentsInChildren<AudioSource>();
+
+        if (source == null || source.Length == 0)
         {
-            GameObject childObject = new GameObject($"{gameObject.name} Audio Source {i}");
-            childObject.transform.parent = transform;
-            childObject.transform.position = Vector3.zero;
-            childObject.transform.rotation = transform.rotation;
-            source[i] = childObject.AddComponent<AudioSource>();
+            source = new AudioSource[concurrentSounds];
+            for (int i = 0; i < concurrentSounds; i++)
+            {
+                GameObject childObject = new GameObject($"{gameObject.name} Audio Source {i}");
+                childObject.transform.parent = transform;
+                childObject.transform.position = Vector3.zero;
+                childObject.transform.rotation = transform.rotation;
+                source[i] = childObject.AddComponent<AudioSource>();
+                source[i].outputAudioMixerGroup = mixer;
+            }
+
+        }else
+        {
+            concurrentSounds = source.Length;
         }
+
 
     }
     public void PlayClip(int clipID)
     {
-        for(int i = 0; i < concurrentSounds; i++)
+        for (int i = 0; i < source.Length; i++)
         {
             if (!source[i].isPlaying)
             {
